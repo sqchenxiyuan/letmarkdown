43f0119b2a084e293d80d45c8e648e1575b643c3
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2017-03-20 12:31:06 +0800

    提取出解析函数

--- src/parse.js
+++ src/parse.js
@@ -1,16 +1,38 @@
 export default function parse(md){
-  // console.log(md);
+
+  let parseData=[];
   let result=[];
   let textArr=md.split('\n');
   let plugins=this.plugins;
-  // console.log(plugins);
+
   if(plugins.length===0)return md;
   while(textArr.length!==0){
     for(let i=0;i<plugins.length;i++){
-        if(plugins[i].check(textArr,result)){
+        let data=plugins[i].check(textArr,parseData);
+        if(data||data===''){
+            if(typeof data === 'string')parseData.push(data);
+            else{
+                 parseData.push({
+                    name:plugins[i].name,
+                    data
+                 });
+            }
+           
             break;
         }
     }
+  }
+
+  loop1:
+  while(parseData.length!==0){
+    for(let i=0;i<plugins.length&&parseData[0].name;i++){
+        if(plugins[i].parse&&plugins[i].name===parseData[0].name){
+            result.push(plugins[i].parse(parseData.shift().data));
+            continue loop1;
+        }
+    }
+
+     result.push(JSON.stringify(parseData.shift()));
   }
 
   return result.join('');
